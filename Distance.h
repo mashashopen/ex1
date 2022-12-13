77edfb2dea64681f19dc8a7082b2ad1ba2910e26
unknown <mashashopen@gmail.com>
2022-12-13 13:00:43 +0200

    fix ;

--- Distance.h
+++ Distance.h
@@ -12,24 +12,11 @@ public:
 	Distance(vector<double> v1, vector<double> v2, string type) {
 		m_v1 = v1;
 		m_v2 = v2;
-<<<<<<< HEAD
-<<<<<<< HEAD
 		m_distanceValue = setRightDistanceFunction(type);
-=======
-		//validation of the input vector and the vector from the classified file: 
-		validationOfTwoVec(m_v1, m_v2);
 
-		m_distName = name;
-		m_distanceValue = setRightDistanceFunction();
->>>>>>> 97f9d21b2f4da3ac03daa821c05a2804645ac825
-	}
-=======
-		m_distanceValue = setRightDistanceFunction(type)
-    
 		//validation of the input vector and the vector from the classified file: 
 		validationOfTwoVec(m_v1, m_v2);
-    }
->>>>>>> 0122b771b6539395ba467aafb4028a6d90837997
+	}
 	void validationOfTwoVec(vector<double> v1, vector<double> v2);
 	bool isWholeNumber();
 	void printDistance();
