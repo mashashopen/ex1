a20e513ce6e3d1dff32a3b9facdbcf39d4b37bca
maayanvikel <maayan.vikel@live.biu.ac.il>
2022-12-14 12:14:43 +0200

    documentation

--- main.cpp
+++ main.cpp
@@ -61,6 +61,12 @@ vector<double> getVectorAsInput() {
     return v;
 }
 
+/*
+* checks if string is actully a integer number.
+*
+* @param s,the string to check.
+* @return true if it is integer, false otherwise.
+*/
 bool isNumber(const string& s)
 {
     for (char const& ch : s) {
@@ -73,7 +79,7 @@ bool isNumber(const string& s)
 /*
 * command line args validation.
 *
-* @param
+* @param argc, argv, k, file, disMetirc- all arguments from cmd.
 * @return true if all args are valid and false otherwise.
 */
 bool areValidArguments(int argc,char* argv[],int& k, string& file, string& disMetric) {
